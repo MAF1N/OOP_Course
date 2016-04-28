b4b245a535bf320f96284cd44800e85aa6bf4c72
MAF1N <nikita_yarovoy@ukr.net>
2016-04-28 11:01:34 +0300

    Added files via upload

---
+++ OOP_Course_Work/OOP_Course_Work/Storage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Course_Work
+{
+
+    class Storage
+    {
+        private Product[] storageProducts = new Product[10000];
+        int lastFree=0;
+        public Storage()
+        {
+
+        }
+        public int StorageCapacity
+        {
+            get { return storageProducts.Length; }
+            private set { }
+        }
+        public int SearchByName(string name)
+        {
+            for (int i = 0; i < lastFree; i++)
+                if (name == storageProducts[i].Name)
+                    return i;
+            return -1;
+        }
+        public int SearchByCode(string c)
+        {
+            for (int i = 0; i < lastFree; i++)
+                if (Convert.ToInt32(c) == storageProducts[i].code)
+                    return i;
+            return -1;
+        }
+        public bool AddProduct(Product p)
+        {
+            if (lastFree < storageProducts.Length)
+            {
+                storageProducts[lastFree] = p;
+                storageProducts[lastFree].code = lastFree;
+                lastFree++;
+                return true;
+            }
+            else return false;
+        }
+        public Product[] GetAllProducts()
+        {
+            Product[] retVal = new Product[lastFree];
+            for (int i = 0; i < lastFree; i++)
+                retVal[i] = storageProducts[i];
+            return retVal;
+        }
+        public int FreePositions()
+        {
+            return storageProducts.Length - lastFree;
+        }
+    }
+}
